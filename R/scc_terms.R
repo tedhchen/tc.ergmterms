5239c999c8cc81377c0fc13edf1e1d657f85d079
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2018-08-16 18:09:50 -0400

    added new wttriple term; old one renamed to wttriple2

--- R/scc_terms.R
+++ R/scc_terms.R
@@ -3,6 +3,23 @@
 
 InitErgmTerm.wttriple<-function (nw, arglist, ...) {
   a <- check.ErgmTerm(nw, arglist, directed=TRUE,
+                      varnames = c("alpha"),
+                      vartypes = c("numeric"),
+                      defaultvalues = list(1),
+                      required = c(FALSE))
+  alpha <- a$alpha
+  tn <- summary.formula(nw~ttriple)
+  coef.names <- "wttriple"
+  inputs <- c(alpha, tn)
+  list(name="wttriple", 
+       coef.names=coef.names, 
+       inputs=inputs, 
+       minval = 0, 
+       pkgname = "tc.ergmterms")
+}
+
+InitErgmTerm.wttriple2<-function (nw, arglist, ...) {
+  a <- check.ErgmTerm(nw, arglist, directed=TRUE,
                       varnames = c("attrname", "diff", "levels", "alpha"),
                       vartypes = c("character", "logical", "character,numeric,logical", "numeric"),
                       defaultvalues = list(NULL, FALSE, NULL, 1),
@@ -12,24 +29,24 @@ InitErgmTerm.wttriple<-function (nw, arglist, ...) {
   alpha <- a$alpha
   tn <- summary.formula(nw~ttriple)
   if(!is.null(attrname)) {
-    nodecov <- get.node.attr(nw, attrname, "wttriple")
+    nodecov <- get.node.attr(nw, attrname, "wttriple2")
     u <- NVL(a$levels, sort(unique(nodecov)))
     if(any(is.na(nodecov))){u<-c(u,NA)}
     nodecov <- match(nodecov,u,nomatch=length(u)+1)
     ui <- seq(along=u)
     if (length(u)==1)
-      stop ("Attribute given to wttriple() has only one value", call.=FALSE)
+      stop ("Attribute given to wttriple2() has only one value", call.=FALSE)
     if (!diff) {
-      coef.names <- paste("wttriple",attrname,sep=".")
+      coef.names <- paste("wttriple2",attrname,sep=".")
       inputs <- c(nodecov)
     } else {
-      coef.names <- paste("wttriple",attrname, u, sep=".")
+      coef.names <- paste("wttriple2",attrname, u, sep=".")
       inputs <- c(ui, nodecov)
       attr(inputs, "ParamsBeforeCov") <- length(ui)
     }
   }else{
-    coef.names <- "wttriple"
+    coef.names <- "wttriple2"
     inputs <- c(alpha, tn)
   }
-  list(name="wttriple", coef.names=coef.names, inputs=inputs, minval = 0, pkgname = "tc.ergmterms")
+  list(name="wttriple2", coef.names=coef.names, inputs=inputs, minval = 0, pkgname = "tc.ergmterms")
 }
--- src-i386/changestats.scc.c
+++ src-i386/changestats.scc.c
@@ -12,6 +12,45 @@
 D_CHANGESTAT_FN(d_wttriple) { 
   Edge e;
   Vertex tail, head, change, node3;
+  int i, old_tn, new_tn;
+  double edgemult;
+  
+  //define tn (ttriple_number) based on INPUT_PARAM[0] supplied from R
+  int tn = INPUT_PARAM[1];
+  
+  /* *** don't forget tail -> head */    
+  ZERO_ALL_CHANGESTATS(i);
+  FOR_EACH_TOGGLE(i) {
+    tail = TAIL(i); head = HEAD(i);
+    edgemult = IS_OUTEDGE(tail, head) ? -1.0 : 1.0;
+    change = 0;
+	old_tn = tn; // old_tn is tn before changes
+    
+	STEP_THROUGH_OUTEDGES(head, e, node3) { /* step through outedges of head */
+      change += IS_INEDGE(node3, tail);
+    }
+    STEP_THROUGH_INEDGES(head, e, node3) {  /* step through inedges of head */
+      change += IS_OUTEDGE(node3, tail) + IS_INEDGE(node3, tail);
+    }
+    
+	//THIS IS THE OLD CODE THAT PRODUCED CORRECT GLOBAL COUNTS
+	//CHANGE_STAT[0] += edgemult * change * pow(INPUT_PARAM[1], INPUT_PARAM[0]) / INPUT_PARAM[1]; 
+	
+	tn += edgemult * change; //calculate changes
+	
+	new_tn = tn; //new_tn is tn after changes
+
+	//update CHANGE_STAT[0] = new_tn^alpha - old_tn^alpha
+	CHANGE_STAT[0] += (pow(new_tn, INPUT_PARAM[0]) - pow(old_tn, INPUT_PARAM[0]));
+	
+    TOGGLE_IF_MORE_TO_COME(i);
+  }
+  UNDO_PREVIOUS_TOGGLES(i);
+}
+
+D_CHANGESTAT_FN(d_wttriple2) { 
+  Edge e;
+  Vertex tail, head, change, node3;
   int i, j;
   double tailattr, edgemult;
   
@@ -55,4 +94,3 @@ D_CHANGESTAT_FN(d_wttriple) {
   }
   UNDO_PREVIOUS_TOGGLES(i);
 }
-
